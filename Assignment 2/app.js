d2bdd1a4dfa2832281ba7a875b408ed4657343d9
Luis Londono <lhl49@cornell.edu>
2019-03-07 20:58:51 -0500

    Finished A2

---
+++ Assignment 2/app.js
@@ -0,0 +1,39 @@
+PORT = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const random = require('random-name');
+const giveJoke = require('give-me-a-joke');
+const path = require('path')
+// const cors = require('cors');
+// VAR IS BAD
+
+// Set up the express app
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+// app.use(cors())
+
+app.get('/api/random-name', function (req, res) {
+    // console.log("Random Name GET request made!")
+    res.send({
+        "first": random.first(),
+        "last": random.last()
+      })
+});
+
+
+app.post('/api/random-joke', function(req,res) {
+    giveJoke.getCustomJoke(req.body.firstName,req.body.lastName, function(joke) {
+        res.send({'joke' : joke});
+        console.log(req.body);
+        })
+})
+
+app.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}`)
+  });
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
